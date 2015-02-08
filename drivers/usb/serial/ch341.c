3de12a809df9eafc39442e746bd886df79478ec8
Karl Palsson <karlp@tweak.net.au>
2015-02-08 02:11:13 +0000

    USB: ch34x: document known chip versions
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- drivers/usb/serial/ch341.c
+++ drivers/usb/serial/ch341.c
@@ -75,8 +75,8 @@
 
 static const struct usb_device_id id_table[] = {
 	{ USB_DEVICE(0x4348, 0x5523) },
-	{ USB_DEVICE(0x1a86, 0x7523) },
-	{ USB_DEVICE(0x1a86, 0x5523) },
+	{ USB_DEVICE(0x1a86, 0x7523) }, /* ch340 */
+	{ USB_DEVICE(0x1a86, 0x5523) }, /* ch341 */
 	{ },
 };
 MODULE_DEVICE_TABLE(usb, id_table);
